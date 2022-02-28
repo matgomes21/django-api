58cab1a4d137f63ac873b020a7e7e4fad7699db1
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-02-27 23:19:45 -0300

    feat(registration): create registration entity

--- school/admin.py
+++ school/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from school.models import Student, Course
+from school.models import Student, Course, Registration
 
 class Students(admin.ModelAdmin):
   list_display = ('id','name','rg','cpf','birth_date')
@@ -15,4 +15,10 @@ class Courses(admin.ModelAdmin):
   search_fields = ('id_course',)
 
 admin.site.register(Course, Courses)
+
+class Registrations(admin.ModelAdmin):
+  list_display = ('id','student','course','period')
+  list_display_links = ('id',)
+
+admin.site.register(Registration, Registrations)
 
