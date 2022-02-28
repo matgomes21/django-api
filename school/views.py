c2db4c5017871091cdd7c8651884f8f0bce83491
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-02-27 23:12:50 -0300

    feat: create serializer, viewsets and routes for courses and students

--- school/views.py
+++ school/views.py
@@ -1,3 +1,17 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from school.models import Student, Course
+from school.serializer import StudentSerializer, CourseSerializer
 
-# Create your views here.
+class StudentsViewSet(viewsets.ModelViewSet):
+  """
+    List all students
+  """
+  queryset = Student.objects.all()
+  serializer_class = StudentSerializer
+
+class CoursesViewSet(viewsets.ModelViewSet):
+  """
+    List all courses
+  """
+  queryset = Course.objects.all()
+  serializer_class = CourseSerializer
