9a942a48d690d77c9e4f141864fa68f75589e644
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-02-27 22:28:01 -0300

    feat(admin): setup admin for students and courses

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from school.models import Student, Course
 
-# Register your models here.
+class Students(admin.ModelAdmin):
+  list_display = ('id','name','rg','cpf','birth_date')
+  list_display_links = ('id','name')
+  search_fields = ('name',)
+  list_per_page = 20
+
+admin.site.register(Student, Students)
+
+class Courses(admin.ModelAdmin):
+  list_display = ('id','id_course','description')
+  list_display_links = ('id','id_course')
+  search_fields = ('id_course',)
+
+admin.site.register(Course, Courses)
+
